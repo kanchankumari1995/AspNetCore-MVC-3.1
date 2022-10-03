3dd835ceb25e582d7d63731039f7eb7621d7ec7e
Kanchan <kanchankumawat1999@gmail.com>
2022-10-03 17:13:35 +0530

    Added libman to use client side library

---
+++ Webgentle.BookStore/Webgentle.BookStore/Repository/BookRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Webgentle.BookStore.Models;
+
+namespace Webgentle.BookStore.Repository
+{
+    public class BookRepository
+    {
+        public List<BookModel> GetAllBooks()
+        {
+            return DataSource();
+        }
+        public BookModel GetBookById(int id)
+        {
+            return DataSource().Where(x => x.id  == id).FirstOrDefault();
+        }
+        public List<BookModel> SearchBook(string title, string authorName)
+        {
+            return DataSource().Where(x => x.Title.Contains(title) && x.Author.Contains(authorName)).ToList();
+        }
+
+        private List<BookModel> DataSource()
+        {
+            return new List<BookModel>()
+            {
+                new BookModel(){id = 1,Title="MVC", Author = "Nitish"},
+                new BookModel(){id = 2,Title="Dot Net Core", Author = "Nitish"},
+                new BookModel(){id = 3,Title="C#", Author = "Monika"},
+                new BookModel(){id = 4,Title="Java", Author = "Webgentle"},
+                new BookModel(){id = 5,Title="Php", Author = "Nitish"},
+            };
+        }
+    }
+}
